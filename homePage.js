ecdf3395f373c571e8ded388e566d290da6ff62e
AhmedKhaled590 <62337087+AhmedKhaled590@users.noreply.github.com>
2021-05-31 13:55:03 +0200

    FrontEnd

---
+++ homePage.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".searchBtn1").click(function () {
+        $(".searchBtn1").hide(1000,function () {
+            $(".SearchBar").show(1000, function () {
+                $(".searchBtn").show(1000, function () {
+                    $(".SearchBar").focus();
+                });
+            });
+        });
+    }) 
+    $(".SearchBar").focusout(function () {
+        $(".searchBtn").fadeOut(1000, function () {
+            $(".SearchBar").hide(1000, function () {
+                $(".searchBtn1").show(1000);
+            });
+            
+        });
+    })
+
+});
