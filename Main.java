96df2dd87753033d29d2db495c3ff71a5dbeb181
YousefAtefB <YousefAtef.b.a.t@gmail.com>
2021-06-01 23:32:56 +0200

    small edit

--- Main.java
+++ Main.java
@@ -5,5 +5,8 @@ public class Main {
         String arr[]=new String[1];
         arr[0]="0";
         InvertedIndexer II = new InvertedIndexer(arr);
+        String str[]=II.Query("cancel");
+        for(int i=0;i<str.length;i++)
+            System.out.println(str[i]);
     }
 }
